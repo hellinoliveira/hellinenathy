e0a700c819d9b6bd2ecc692d4489591942fe7113
hellison <hellison.oliveira@gmail.com>
2015-03-12 21:29:22 -0300

    padrinhos

--- js/fb.js
+++ js/fb.js
@@ -1,18 +1,30 @@
-window.fbAsyncInit = function () {
+window.fbAsyncInit = function() {
     FB.init({
-        appId: 'your-app-id',
-        xfbml: true,
-        version: 'v2.1'
+        appId      : '797118780374988',
+        xfbml      : true,
+        version    : 'v2.2'
     });
+    FB.api(
+        "/4/picture",
+        {
+            "redirect": false,
+            "height": 200,
+            "width": 200,
+            "type": "normal"
+        },
+        function (response) {
+            if (response && !response.error) {
+                console.log(response)
+                $('#fb-photo').html(response)
+            }
+        }
+    );
 };
 
-(function (d, s, id) {
+(function(d, s, id){
     var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
     js.src = "//connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
